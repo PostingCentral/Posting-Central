5578c27f87db8f1ef601cf4afd699f03d403a29f
Immanuel Salva <83900884+ImmanuelSalva@users.noreply.github.com>
2021-07-22 09:09:20 +0700

    Add files via upload

---
+++ about-me.php
@@ -0,0 +1,34 @@
+<?php
+  $results['pageTitle'] = "About - Mein Bloggen";
+?>
+<?php
+include ("templates/include/header.php")
+?>
+    <h1 style="font-size: 60px;">About</h1>
+    <p style="width: 40%; font-size: 20px;">
+        So You Might Be Wondering "What is Mein Bloggen?" Well, Mein Bloggen is a Blog
+        Run By Me; Immanuel Salva. I Use This Blog to Post Arts, Stories, Videos and Other 
+        Stuff. And if you're Wondering if I'm Using Wordpress Then NO, I'm Not Using Wordpress
+        I Made my own CMS Because Wordpress Takes a lotta Space, So I'm a Pretty Good Coder
+        Too. I am a Left-wing Political Activist That's Anti-Communist and Pro-Democratic-Socialist.
+        And This is a Company Subdomain from one of My Companies so i'm also an Entrepreneuer and CEO
+        of My Own Company.
+    <h1 style="font-size: 60px;">What's In This Blog</h1>
+    <p style="width: 40%; font-size: 20px;">
+        Well To Explain What's In This Blog then Look at My Posts; It's Just a Categorized
+        Mess of Posts. But to Go In Eveb Deeper Then I'll Explain: <br>
+        This Blog is Used as a Video Blog and an Artist Blog Whilst at The Same Time It's also
+        a Political and Story Blog. The Categorized Mess That i Mean is The Fact That Due to My CMS's 
+        Limited Capibility Sometimes I Just Post Something and It Will Become a Huge Mess for Anyone
+        To Look/Read to.
+    </p>
+    <h1 style="font-size: 60px;">What's My Main Language</h1>
+    <p style="width: 40%; font-size: 20px;">
+        My Language Is Definetely Not Japanese and if i we're Then I Would've Spelled Hello With Herro
+        and Say 'Igirisu' Instead of England, so Japanese is Checked out as a NO. I Speak The Cousin 
+        Language of Malay and I don't mean Hindi. and yes Things Don't Rhyme But i Guess it's Fine, 
+        So If You Guessed My Language Then Keep Your Mouth Shut.
+    </p>
+<?php
+include ("templates/include/footer.php")
+?>
---
+++ contact-me.php
@@ -0,0 +1,10 @@
+<?php
+$results['pageTitle'] = "Contact - Mein Bloggen";
+?>
+<?php
+include ("templates/include/header.php")
+?>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdnCNsr-JEkn_1_YuRDapU_cdC6SDhgT9oNHdarU3ATbNDAAg/viewform?embedded=true" width="100%" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+<?php
+include ("templates/include/footer.php")
+?>
